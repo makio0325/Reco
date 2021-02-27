37626dff9fedc10e6016c4b8747a4941fc1573b1
makio0325 <73296055+makio0325@users.noreply.github.com>
2021-02-28 00:26:26 +0900

    admobテスト

--- App.jsx
+++ App.jsx
@@ -1,14 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
+import { AdMobBanner } from 'expo-ads-admob';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <AdMobBanner 
+        adUnitID="ca-app-pub-3940256099942544/2934735716"
+        style={styles.banner}
+        />
     </View>
+
   );
 }
 
@@ -19,4 +26,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  banner: {
+    alignItems: 'center',
+    position:'absolute',
+    bottom: 50,
+  }
 });
